494df934e1b4ac343344aea6455ceb4f453661d4
Shane Linden <shanelinden1995@gmail.com>
2022-08-14 10:22:36 +0200

    Adds Book POJO

--- src/main/java/com/bookshopautomation/models/Book.java
+++ src/main/java/com/bookshopautomation/models/Book.java
@@ -1,4 +1,28 @@
 package com.bookshopautomation.models;
 
+import java.math.BigDecimal;
+import java.time.Year;
+
 public class Book {
+  private String title;
+  private Year year;
+  private BigDecimal price;
+
+  public Book(String title, Year year, BigDecimal price) {
+    this.title = title;
+    this.year = year;
+    this.price = price;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public Year getYear() {
+    return year;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
 }
