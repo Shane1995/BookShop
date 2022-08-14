f08064b4b0f479b618f02d550487c2b838964055
Shane Linden <shanelinden1995@gmail.com>
2022-08-14 10:23:36 +0200

    Adds on year discount type.

---
+++ src/main/java/com/bookshopautomation/discountProvider/discountTypes/OnYearImpl.java
@@ -0,0 +1,37 @@
+package com.bookshopautomation.discountProvider.discountTypes;
+
+import com.bookshopautomation.models.Book;
+import com.bookshopautomation.models.CheckoutOrder;
+
+import java.math.BigDecimal;
+import java.time.Year;
+
+public class OnYearImpl extends Discount {
+
+  private Year yearClause = Year.parse("2000");
+  private int discountPercentage = 10;
+
+  @Override
+  public void setDiscountPercentage(int percentage) {
+    this.discountPercentage = percentage;
+  }
+
+  @Override
+  public <T> void updateDiscountClause(T value) {
+    yearClause = (Year) value;
+  }
+
+  @Override
+  public CheckoutOrder applyCondition(CheckoutOrder checkoutOrder) {
+    BigDecimal deductibleDiscountTotal = new BigDecimal("00");
+
+    for (Book book:checkoutOrder.getItems()) {
+      if (book.getYear().isAfter(yearClause)) {
+        deductibleDiscountTotal = deductibleDiscountTotal.add(book.getPrice().divide(BigDecimal.valueOf(discountPercentage)));
+      }
+    }
+
+    BigDecimal updatedTotal = checkoutOrder.getTotalPrice().subtract(deductibleDiscountTotal);
+    return new CheckoutOrder(checkoutOrder.getItems(), updatedTotal);
+  }
+}
