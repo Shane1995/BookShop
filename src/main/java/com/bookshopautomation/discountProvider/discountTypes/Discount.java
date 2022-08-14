18440a51c13271471d0ffb00bc202348abf81e5c
Shane Linden <shanelinden1995@gmail.com>
2022-08-14 11:51:48 +0200

    Update to abstact class, adds common shared methods for discount types.

--- src/main/java/com/bookshopautomation/discountProvider/discountTypes/Discount.java
+++ src/main/java/com/bookshopautomation/discountProvider/discountTypes/Discount.java
@@ -3,10 +3,21 @@ package com.bookshopautomation.discountProvider.discountTypes;
 import com.bookshopautomation.discountProvider.exceptions.DiscountPercentageException;
 import com.bookshopautomation.models.CheckoutOrder;
 
-public interface Discount {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-  void setDiscountPercentage(int percentage);
+public abstract class Discount {
 
-  <T> void updateDiscountClause(T value);
-  CheckoutOrder applyCondition(CheckoutOrder checkoutOrder) throws DiscountPercentageException;
+  abstract public void setDiscountPercentage(int percentage);
+  abstract public <T> void updateDiscountClause(T value);
+  abstract public CheckoutOrder applyCondition(CheckoutOrder checkoutOrder) throws DiscountPercentageException;
+
+  public BigDecimal calculateDeductibleDiscount(BigDecimal price, int discountPercentage){
+    float percentage = discountPercentage/100f;
+    return price.multiply(BigDecimal.valueOf(percentage));
+  }
+
+  public BigDecimal calculateUpdatedTotalCost(BigDecimal oldTotalPrice, BigDecimal totalDeductibleDiscount){
+    return oldTotalPrice.subtract(totalDeductibleDiscount);
+  }
 }
