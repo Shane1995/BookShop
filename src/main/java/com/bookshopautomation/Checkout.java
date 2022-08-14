ded9370e970b45b5cff7c808ffb02646977df541
Shane Linden <shanelinden1995@gmail.com>
2022-08-14 10:34:13 +0200

    Adds Discount provider abstraction to the checkout class.

--- src/main/java/com/bookshopautomation/Checkout.java
+++ src/main/java/com/bookshopautomation/Checkout.java
@@ -1,12 +1,22 @@
 package com.bookshopautomation;
 
+import com.bookshopautomation.discountProvider.DiscountProvider;
+import com.bookshopautomation.discountProvider.discountTypes.Discount;
 import com.bookshopautomation.models.Book;
+import com.bookshopautomation.models.CheckoutOrder;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class Checkout {
-  public BigDecimal checkoutItems(List<Book> books){
+  private final DiscountProvider discountProvider;
+
+  public Checkout(DiscountProvider discountProvider) {
+    this.discountProvider = discountProvider;
+  }
+
+  public BigDecimal checkoutItems(List<Book> books) {
     return null;
   }
 }
